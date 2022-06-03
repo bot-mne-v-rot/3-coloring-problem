b198ebfb4f70cc279e10e3cf460a12d4307eb68c
SmnTin <smn.pankv@gmail.com>
2022-06-03 05:58:52 +0300

    Refactor Reduction.hs

--- src/Reduction.hs
+++ src/Reduction.hs
@@ -1,113 +1,78 @@
 module Reduction (
-    Literal,
+    Var(Var),
+    Literal(Lit, NLit),
     Clause,
     CNF,
 
-    Vertex,
-    Vertexes,
     Edges,
-    Graph,
+    Graph(Graph),
 
-    Color,
+    Color(R, G, B),
     Coloring,
     Solution,
-    Solver,
+    SatSolver,
 
     generateCNF,
     recoverAnswer,
     solve
 ) where
+    
+import Control.Monad
+import Control.Applicative
 
-type Literal = Int
-type Clause  = [Literal]
-type CNF     = [Clause]
+type Edges v = [(v, v)]
+data Graph v = Graph { getVertices :: [v], getEdges :: Edges v }
 
-type Vertex = Int
-type Vertexes = [Vertex]
-type Edges  = [(Vertex, Vertex)]
-data Graph = RecordType { size :: Int, vertexes :: Vertexes, edges :: Edges }
+data Var v = Var v Color
+    deriving (Show, Eq)
+
+data Literal v  = Lit (Var v) | NLit (Var v)
+type Clause v   = [Literal v]
+type CNF v      = [Clause v]
+type Solution v = [(Var v, Bool)]
 
 data Color = R | G | B
-    deriving (Show, Enum)
+    deriving (Show, Enum, Eq)
 
 type Coloring = [Color]
 
-newtype Solution = Solution [Int]
-    deriving (Show, Eq, Ord)
+type SatSolver v = CNF v -> Maybe (Solution v)
 
-type Solver = CNF -> Maybe Solution
+solve :: Eq v => SatSolver v -> Graph v -> Maybe Coloring
+solve solver graph = recoverAnswer graph $ solver $ generateCNF graph
 
-solve :: Solver -> Graph -> Coloring
-solve solver graph = recoverAnswer (size graph) $ solver $ generateCNF' graph
+recoverAnswer :: Eq v => Graph v -> Maybe (Solution v) -> Maybe Coloring
+recoverAnswer _ Nothing = Nothing
+recoverAnswer graph (Just xs) =
+    mapM f (getVertices graph)
+    where f v = do
+            let col c = do
+                val <- lookup (Var v c) xs
+                guard val
+                return c
+            col R <|> col G <|> col B
 
-recoverAnswer :: Int -> Maybe Solution -> Coloring
-recoverAnswer _ Nothing = []
-recoverAnswer _ (Just (Solution [])) = []
-recoverAnswer n (Just (Solution xs)) = helper n xs []
-    where helper n [] res = res
-          helper n (x:xs) res | x < 0 = helper n xs res
-                              | otherwise = helper n xs (colorByLiteral n x : res)
-
-colorByLiteral :: Int -> Int -> Color
-colorByLiteral n lit = toEnum $ snd $ fromSingleToPair n lit
-
-fromSingleToPair :: Int -> Int -> (Int, Int)
-fromSingleToPair n idx = ((idx - 1) `div` n, (idx - 1) `mod` n)
-
-generateCNF' :: Graph -> CNF
-generateCNF' graph = let sz = size graph in vertexHasColor sz ++ vertexesColorBound sz ++ edgesHasDiffColors sz (edges graph)
-
-generateCNF :: Int -> Edges -> CNF
-generateCNF n edges = vertexHasColor n ++ vertexesColorBound n ++ edgesHasDiffColors n edges
+generateCNF :: Graph v -> CNF v
+generateCNF g =
+    vertexHasColor g ++
+    verticesColorUniqueness g ++
+    adjVerticesHaveDiffColors g
 
 {- Clauses for condition: each vertex has at least one color -}
-vertexHasColor :: Int -> CNF
-vertexHasColor n = do
-    vertex <- [0..(n - 1)]
-    let clause = [ fromPairToSingle n (vertex, color) | color <- [0..2] ]
-    return clause
+vertexHasColor :: Graph v -> CNF v
+vertexHasColor g = do
+    v <- getVertices g
+    return [ Lit $ Var v c | c <- [R, G, B] ]
 
-{- Clauses for condition: each vertex has not more than one color -}
-vertexesColorBound :: Int -> CNF
-vertexesColorBound n = helper n (n - 1) []
-    where helper n i list | i >= 0    = helper n (i - 1) (vertexBoundColor n i ++ list)
-                          | otherwise = list
+{- Clauses for condition: no more than one color is assigned to each vertex -}
+verticesColorUniqueness :: Graph v -> CNF v
+verticesColorUniqueness g = do
+    v <- getVertices g
+    let diffCols c1 c2 = [ NLit $ Var v c1, NLit $ Var v c2 ]
+    [ diffCols R B, diffCols B G, diffCols R G ]
 
-vertexBoundColor :: Int -> Vertex -> CNF
-vertexBoundColor n i = do
-    f <- getPossibleColors i
-    s <- getPossibleColors i
-    let flit = fromPairToSingle n f
-    let slit = fromPairToSingle n s
-    if flit < slit && (flit /= slit)
-        then return [-flit, -slit] -- by De Morgans Law
-        else []
-
-getPossibleColors :: Vertex -> [(Int, Int)]
-getPossibleColors i = [ (i, j) | j <- [0..2] ]
-
-{- Clauses for condition: each edge's vertexes has different colors -}
-edgesHasDiffColors :: Int -> Edges -> CNF
-edgesHasDiffColors n edges = do
-    e <- getPossibleEdges n
-    f <- getPossibleColors (fst e)
-    s <- getPossibleColors (snd e)
-    if snd f == snd s
-        then do
-            let flit = fromPairToSingle n f
-            let slit = fromPairToSingle n s
-            if flit /= slit && (e `elem` edges)
-                then return [-flit, -slit] -- by De Morgans Law
-                else []
-        else []
-
-getPossibleEdges :: Int -> Edges
-getPossibleEdges n = do
-                    i <- [0..(n - 1)]
-                    j <- [0..(n - 1)]
-                    if i /= j then return (i, j)
-                    else []
-
-fromPairToSingle :: Int -> (Int, Int) -> Int
-fromPairToSingle n (i, j) = n * i + j + 1
-
+{- Clauses for condition: adjacent vertices have different colors -}
+adjVerticesHaveDiffColors :: Graph v -> CNF v
+adjVerticesHaveDiffColors g = do
+    (u, v) <- getEdges g
+    [ [ NLit $ Var u c, NLit $ Var v c ] | c <- [R, G, B] ]
